03fc5e0f4acc49a4e6a2649bfcae73165d1e177a
Hunddy <58757071+hunddy@users.noreply.github.com>
2020-03-13 12:15:27 -0700

    Update beg.js

--- commands/economy/beg.js
+++ commands/economy/beg.js
@@ -14,17 +14,17 @@ module.exports = {
         let user = message.author;
 
         let timeout = 180000;
-        let amount = Math.floor(Math.random() * 200) + 1; // 1-500 random number. whatever you'd like
+        let amount = 100;
       
         let beg = await db.fetch(`beg_${message.guild.id}_${user.id}`);
       
         if (beg !== null && timeout - (Date.now() - beg) > 0) {
           let time = ms(timeout - (Date.now() - beg));
         var beggingrandom = [
-            "You were looking in the dumpster at microsoft trying find stuff you can sell Bill gates came out of no where and handed you `${amount}`",
-            "You walked into the apple store with the sign asking for money luckily Steven Paul was there he handed you `${amount}`",
-            "As Obama was driving by he spotted you on the street holding a sign asking for money, he feels bad for you so he gives you `${amount}`",
-            "A random dude was selling jewlery he asked you if you wanted to buy some. But once he looked at you and your condition he gave you `${amount}`"
+            "You were looking in the dumpster at microsoft trying find stuff you can sell Bill gates came out of no where and handed you 100 Animebucks",
+            "You walked into the apple store with the sign asking for money luckily Steven Paul was there he handed you 100 Animebucks",
+            "As Obama was driving by he spotted you on the street holding a sign asking for money, he feels bad for you so he gives you 100 Animebucks",
+            "A random dude was selling jewlery he asked you if you wanted to buy some. But once he looked at you and your condition he gave you 100 Animebucks"
         ];
             var begg = Math.floor(Math.random() * beggingrandom.length);
         
