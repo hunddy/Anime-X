8dcf980ec4f3fa0a56f490d71ef3c2ad9ee2ec5f
Hunddy <58757071+hunddy@users.noreply.github.com>
2020-03-13 16:51:41 -0700

    Update dailybucks.js

--- commands/economy/dailybucks.js
+++ commands/economy/dailybucks.js
@@ -1,30 +1,40 @@
 module.exports = {
     name: "dailybucks",
     category: "economy",
+    aliases: ["daily", "dailybucks", "dailybuck"],
     description: "This command is used for gaining money.",
     run: async (client, message, args) => {
         const {Client, RichEmbed, Collection} = require('discord.js')
-        const db = require('quick.db');
-        const ms = require('ms');
+
         let timeout = 86400000 // 24 hours in milliseconds, change if you'd like.
-    let amount = 500
-    // random amount: Math.floor(Math.random() * 1000) + 1;
+         const db = require('quick.db');
+         let user = message.mentions.members.first() || message.author;
+         let vip = db.fetch(`vip_${message.guild.id}_${user.id}`)
+         if(vip === true) vip = 500 + 250;
+         if (vip === null) vip = 500 + 0;
 
 
-    let daily = db.fetch(`daily_${message.guild.id}_${message.author.id}`)
+        const ms = require('parse-ms');
+               
 
-    if (daily !== null && timeout - (Date.now() - daily) > 0) {
-        let time = ms(timeout - (Date.now() - daily));
-
-        message.channel.send(`❌ **You already collected ur daily reward, you can come back and collect it in 24 HOURS**!`)
+        if(!message.content.startsWith('?'))return;  
+      
+        let daily = await db.fetch(`beg_${message.guild.id}_${user.id}`);
+      
+        if (daily !== null && timeout - (Date.now() - daily) > 0) {
+          let time = ms(timeout - (Date.now() - daily));
+           let timeEmbed = new RichEmbed()
+          .setColor("RANDOM")
+          .setDescription(`:x: You've already begged recently\n\nYou wil be ablle to claim you daiybucks in again in ${time.minutes}m ${time.seconds}s.>`);
+          message.channel.send(timeEmbed)
     } else {
     let embed = new RichEmbed()
     .setColor("RANDOM")
     .setDescription(`**Daily Reward**`)
-    .addField('**Collected**', amount)
+    .addField('**Collected**', vip)
 
     message.channel.send(embed)
-    db.add(`animebucks_${message.guild.id}_${message.author.id}`, amount)
+    db.add(`animebucks_${message.guild.id}_${message.author.id}`, vip)
     db.set(`daily_${message.guild.id}_${message.author.id}`, Date.now())
 
     }
