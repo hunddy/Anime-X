db674615ee52b95b4b34c8b74ba95f6251a9ceb2
Hunddy <58757071+hunddy@users.noreply.github.com>
2020-03-13 15:59:48 -0700

    Update beg.js

--- commands/economy/beg.js
+++ commands/economy/beg.js
@@ -5,12 +5,14 @@ module.exports = {
     description: "This command is used for begging money POOR.",
     run: async (client, message, args) => {
         const {Client, RichEmbed, Collection} = require('discord.js')
-         let amount = Math.floor(Math.random() * 500) + 1; // 1-500 random number. whatever you'd like
+         let vip = db.fetch(`vip_${message.guild.id}_${user.id}`)
+         if(vip === true) random = Math.floor(Math.random() * 350) + 1;
+         if (vip === null) random = Math.floor(Math.random() * 200) + 1;
          var beggingrandom = [
-            `You were looking in the dumpster at microsoft trying find stuff you can sell Bill Gates came out of no where and handed you __**${amount}**__ Animebucks`,
-            `You walked into the apple store with the sign asking for money luckily Steven Paul was there he handed you __**${amount}**__ Animebucks`,
-            `As Barack Obama was driving by he spotted you on the street holding a sign asking for money, he feels bad for you so he gives you __**${amount}**__ Animebucks`,
-            `A random dude was selling jewlery he asked you if you wanted to buy some. But once he looked at you and your condition he gave you __**${amount}**__ Animebucks`
+            `You were looking in the dumpster at microsoft trying find stuff you can sell Bill Gates came out of no where and handed you __**${vip}**__ Animebucks`,
+            `You walked into the apple store with the sign asking for money luckily Steven Paul was there he handed you __**${vip}**__ Animebucks`,
+            `As Barack Obama was driving by he spotted you on the street holding a sign asking for money, he feels bad for you so he gives you __**${vip}**__ Animebucks`,
+            `A random dude was selling jewlery he asked you if you wanted to buy some. But once he looked at you and your condition he gave you __**${vip}**__ Animebucks`
         ];
         let begrandomm = (beggingrandom[Math.floor(Math.random() *beggingrandom.length)]);
         const db = require('quick.db');
@@ -39,7 +41,7 @@ module.exports = {
         .setColor("RANDOM")
         .addField("**Situation**", begrandomm);
         message.channel.send(moneyEmbed)
-        db.add(`animebucks_${message.guild.id}_${user.id}`, amount)
+        db.add(`animebucks_${message.guild.id}_${user.id}`, vip)
         db.set(`beg_${message.guild.id}_${user.id}`, Date.now())
       
       
