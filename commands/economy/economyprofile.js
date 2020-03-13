b3f126430d0dd23052b1a444fc5c804ec059ce01
Hunddy <58757071+hunddy@users.noreply.github.com>
2020-03-13 13:48:04 -0700

    Update economyprofile.js

--- commands/economy/economyprofile.js
+++ commands/economy/economyprofile.js
@@ -1,6 +1,7 @@
 module.exports = {
     name: "eprofile",
     category: "economy",
+    aliases: ["eprofile", "economyprofile", "inventory"],
     description: "This command is used for seeing other economy status.",
     run: async (client, message, args) => {
         const {Client, RichEmbed, Collection} = require('discord.js')
@@ -9,26 +10,13 @@ module.exports = {
         if(!message.content.startsWith('?'))return; 
 
         let user = message.mentions.members.first() || message.author;
-        
-        let education = await db.fetch(`education_${message.guild.id}_${user.id}`)
-        if (education === null) education = 0;
-
-        let money = await db.fetch(`animebucks_${message.guild.id}_${user.id}`)
-        if (money === null) money = 0;
-      
-       let bank = await db.fetch(`bank_${message.guild.id}_${user.id}`)
-       if (bank === null) bank = 0;
-
-        let vip = await db.fetch(`vip_${message.guild.id}_${user.id}`)
-          if(vip === null) vip = 'None'
-          if(vip === true) vip = 'VIP'
       
         let shoes = await db.fetch(`jordans_${message.guild.id}_${user.id}`)
         if(shoes === null) shoes = '0'
       
         let moneyEmbed = new RichEmbed()
         .setColor("RANDOM")
-        .setDescription(`**${user}'s Profile**\n\n**Animebucks: ${money}\nBank: ${bank}\nEducation hours: ${education}\nVIP Rank: ${vip}\n\nInventory\n\njordans: ${shoes}**`);
+        .setDescription(`**${user}'s Profile**\n\nInventory\n\njordans: ${shoes}**`);
         message.channel.send(moneyEmbed)
       
     }
