848562542d6bb2f094a628803b931fd6d40cccee
Hunddy <58757071+hunddy@users.noreply.github.com>
2020-03-15 12:24:42 -0700

    Update buymoneycrate.js

--- commands/economy/buymoneycrate.js
+++ commands/economy/buymoneycrate.js
@@ -7,7 +7,7 @@ module.exports = {
         const {Client, RichEmbed, Collection} = require('discord.js')
         const db = require('quick.db');
         let user = message.author;
-        let author = db.fetch(`animebucks_${message.guild.id}_${message.author.id}`)
+        let author = db.fetch(`animebucks_${message.author.id}`)
         if(!message.content.startsWith('?'))return; 
 
         let Embed2 = new RichEmbed()
@@ -16,14 +16,14 @@ module.exports = {
 
         if (author < 1000) return message.channel.send(Embed2)
        
-        db.fetch(`moneycrate_${message.guild.id}_${user.id}`)
-        db.add(`moneycrate_${message.guild.id}_${user.id}`, 1)
+        db.fetch(`moneycrate_${message.author.id}`)
+        db.add(`moneycrate_${message.author.id}`, 1)
 
         let Embed3 = new RichEmbed()
         .setColor("RANDOM")
         .setDescription(`:white_check_mark: Purchased Moneycrate! :briefcase: For 1000 Animebucks, to open the crate do ?openmoneycrate`);
 
-        db.subtract(`animebucks_${message.guild.id}_${user.id}`, 1000)
+        db.subtract(`animebucks_${message.author.id}`, 1000)
         message.channel.send(Embed3)
     }
 }
