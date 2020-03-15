325b421ced5c3a03fced312d189a30659192b66a
Hunddy <58757071+hunddy@users.noreply.github.com>
2020-03-15 12:15:46 -0700

    Update buy.js

--- commands/economy/buy.js
+++ commands/economy/buy.js
@@ -6,7 +6,7 @@ module.exports = {
         const {Client, RichEmbed, Collection} = require('discord.js')
         const db = require('quick.db');
         let user = message.author;
-        let author = db.fetch(`animebucks_${message.guild.id}_${message.author.id}`)
+        let author = db.fetch(`animebucks_${message.author.id}`)
         if(!message.content.startsWith('?'))return; 
         
         if (args[0] == 'moneycrate') {
@@ -16,14 +16,14 @@ module.exports = {
 
         if (author < 1000) return message.channel.send(moneycrate2)
        
-        db.fetch(`moneycrate_${message.guild.id}_${user.id}`)
-        db.add(`moneycrate_${message.guild.id}_${user.id}`, 1)
+        db.fetch(`moneycrate_${message.author.id}`)
+        db.add(`moneycrate_${message.author.id}`, 1)
 
         let moneycrate3 = new RichEmbed()
         .setColor("RANDOM")
         .setDescription(`:white_check_mark: Purchased Moneycrate! :briefcase: For 1000 Animebucks, to open the crate do ?open moneycrate`);
 
-        db.subtract(`animebucks_${message.guild.id}_${user.id}`, 1000)
+        db.subtract(`animebucks_${message.author.id}`, 1000)
         message.channel.send(moneycrate3)
         } else if(args[0] == 'jordans') {
                let jordans2 = new RichEmbed()
@@ -32,14 +32,14 @@ module.exports = {
 
         if (author < 600) return message.channel.send(jordans2)
        
-        db.fetch(`jordans_${message.guild.id}_${user.id}`)
-        db.add(`jordans_${message.guild.id}_${user.id}`, 1)
+        db.fetch(`jordans_${message.author.id}`)
+        db.add(`jordans_${message.author.id}`, 1)
 
         let jordans3 = new RichEmbed()
         .setColor("RANDOM")
         .setDescription(`:white_check_mark: Purchased Fresh jordans <:jordans:679200006942490654> For 600 Animebucks`);
 
-        db.subtract(`animebucks_${message.guild.id}_${user.id}`, 600)
+        db.subtract(`animebucks_${message.author.id}`, 600)
         message.channel.send(jordans3)
         } else if(args[0] == 'vip') {
      let vip1 = new RichEmbed()
@@ -50,15 +50,15 @@ module.exports = {
     let vip2 = new RichEmbed()
     .setColor("RANDOM")
     .setDescription(`You already owned vip you can't purchase it again.`)
-    if(db.fetch(`vip_${message.guild.id}_${user.id}`)) return message.channel.send(vip2)
-        db.fetch(`vip_${message.guild.id}_${user.id}`);
-        db.set(`vip_${message.guild.id}_${user.id}`, true)
+    if(db.fetch(`vip_${message.author.id}`)) return message.channel.send(vip2)
+        db.fetch(`vip_${message.author.id}`);
+        db.set(`vip_${message.author.id}`, true)
 
         let vip3 = new RichEmbed()
         .setColor("RANDOM")
         .setDescription(`:white_check_mark: Purchased VIP <:Crown:679163396750442535> For 3500 Animebucks`);
 
-        db.subtract(`animebucks_${message.guild.id}_${user.id}`, 3500)
+        db.subtract(`animebucks_${message.author.id}`, 3500)
         message.channel.send(vip3)
         }
         
