1a0ef43a30ecdefaab52062be595f7a0ff9f5aa9
Hunddy <58757071+hunddy@users.noreply.github.com>
2020-03-15 12:26:33 -0700

    Update buyvip.js

--- commands/economy/buyvip.js
+++ commands/economy/buyvip.js
@@ -6,7 +6,7 @@ module.exports = {
         const {Client, RichEmbed, Collection} = require('discord.js')
         const db = require('quick.db');
         let user = message.author;
-        let author = db.fetch(`animebucks_${message.guild.id}_${message.author.id}`)
+        let author = db.fetch(`animebucks_${message.author.id}`)
 
         if(!message.content.startsWith('?'))return; 
 
@@ -19,15 +19,16 @@ module.exports = {
     let Embed3 = new RichEmbed()
     .setColor("RANDOM")
     .setDescription(`You already owned vip you can't purchase it again.`)
-    if(db.fetch(`vip_${message.guild.id}_${user.id}`)) return message.channel.send(Embed3)
-        db.fetch(`vip_${message.guild.id}_${user.id}`);
-        db.set(`vip_${message.guild.id}_${user.id}`, true)
+    if(db.fetch(`vip_${message.author.id}`)) return message.channel.send(Embed3)
+        
+        db.fetch(`vip_${message.author.id}`);
+        db.set(`vip_${message.author.id}`, true)
 
         let Embed2 = new RichEmbed()
         .setColor("RANDOM")
         .setDescription(`:white_check_mark: Purchased VIP <:Crown:679163396750442535> For 3500 Animebucks`);
 
-        db.subtract(`animebucks_${message.guild.id}_${user.id}`, 3500)
+        db.subtract(`animebucks_${message.author.id}`, 3500)
         message.channel.send(Embed2)
     
     }
