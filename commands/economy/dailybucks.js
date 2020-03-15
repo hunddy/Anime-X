a81d88b1c9ff9bfb44752e786fdbb7c6836ca248
Hunddy <58757071+hunddy@users.noreply.github.com>
2020-03-15 12:31:01 -0700

    Update dailybucks.js

--- commands/economy/dailybucks.js
+++ commands/economy/dailybucks.js
@@ -8,8 +8,8 @@ module.exports = {
 
         let timeout = 86400000 // 24 hours in milliseconds, change if you'd like.
          const db = require('quick.db');
-         let user = message.mentions.members.first() || message.author;
-         let vip = db.fetch(`vip_${message.guild.id}_${user.id}`)
+         let user = message.author;
+         let vip = db.fetch(`vip_${message.author.id}`)
          if(vip === true) vip = 500 + 250;
          if (vip === null) vip = 500 + 0;
 
@@ -19,7 +19,7 @@ module.exports = {
 
         if(!message.content.startsWith('?'))return;  
       
-        let daily = await db.fetch(`beg_${message.guild.id}_${user.id}`);
+        let daily = await db.fetch(`daily_${message.author.id}`);
       
         if (daily !== null && timeout - (Date.now() - daily) > 0) {
           let time = ms(timeout - (Date.now() - daily));
@@ -35,8 +35,8 @@ module.exports = {
     .addField('**Collected**', vip)
 
     message.channel.send(embed)
-    db.add(`animebucks_${message.guild.id}_${message.author.id}`, vip)
-    db.set(`daily_${message.guild.id}_${message.author.id}`, Date.now())
+    db.add(`animebucks_${message.author.id}`, vip)
+    db.set(`daily_${message.author.id}`, Date.now())
 
     }
 }
