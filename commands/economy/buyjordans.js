f2ca91025ae6913508ce9f76b0d89ba29f11f84b
Hunddy <58757071+hunddy@users.noreply.github.com>
2020-03-15 12:22:13 -0700

    Update buyjordans.js

--- commands/economy/buyjordans.js
+++ commands/economy/buyjordans.js
@@ -6,7 +6,7 @@ module.exports = {
         const {Client, RichEmbed, Collection} = require('discord.js')
         const db = require('quick.db');
         let user = message.author;
-        let author = db.fetch(`animebucks_${message.guild.id}_${message.author.id}`)
+        let author = db.fetch(`animebucks_${message.author.id}`)
         if(!message.content.startsWith('?'))return; 
 
         let Embed2 = new RichEmbed()
@@ -15,14 +15,14 @@ module.exports = {
 
         if (author < 600) return message.channel.send(Embed2)
        
-        db.fetch(`jordans_${message.guild.id}_${user.id}`)
-        db.add(`jordans_${message.guild.id}_${user.id}`, 1)
+        db.fetch(`jordans_${message.author.id}`)
+        db.add(`jordans_${message.author.id}`, 1)
 
         let Embed3 = new RichEmbed()
         .setColor("RANDOM")
         .setDescription(`:white_check_mark: Purchased Fresh jordans <:jordans:679200006942490654> For 600 Animebucks`);
 
-        db.subtract(`animebucks_${message.guild.id}_${user.id}`, 600)
+        db.subtract(`animebucks_${message.author.id}`, 600)
         message.channel.send(Embed3)
     }
 }
