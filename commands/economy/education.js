e9df488a56ddf1c0daf8cbd20c2a89c6d0950e8c
Hunddy <58757071+hunddy@users.noreply.github.com>
2020-03-16 10:00:41 -0700

    Update education.js

--- commands/economy/education.js
+++ commands/economy/education.js
@@ -6,11 +6,12 @@ module.exports = {
         const {Client, RichEmbed, Collection} = require('discord.js')
         const db = require('quick.db');
                
-        let education = await db.fetch(`education_${message.author.id}`)
+        let education = await db.fetch(`educations_${message.author.id}`)
 
         if (education === null) education = 0
         let educationhours = new RichEmbed()
         .setTitle('Education/school minutes')
+        .addField('**TIP**', 'Once, you have enough minutes of school you will be able to graduate and get a High school diploma.')
         .setDescription(`You have ${education} minutes of school. To go to school use the command "?school`)
         message.channel.send(educationhours)
         
