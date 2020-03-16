b02469e26184b9a3a3f8e86c53f75be1d30899d5
Hunddy <58757071+hunddy@users.noreply.github.com>
2020-03-16 10:34:52 -0700

    Update education.js

--- commands/economy/education.js
+++ commands/economy/education.js
@@ -7,12 +7,16 @@ module.exports = {
         const db = require('quick.db');
                
         let education = await db.fetch(`educations_${message.author.id}`)
+       let graduation = await db.fetch(`highschooldiploma_${message.author.id}`)
+        if(graduation = null) graduation = "Not yet graduated";
+        if(graduation = true) graduation = "High school diploma";
 
         if (education === null) education = 0
         let educationhours = new RichEmbed()
         .setTitle('Education/school minutes')
-        .addField('**TIP**', 'Once, you have enough minutes of school you will be able to graduate and get a High school diploma.')
+        .addField('**TIP**', 'Once, you have enough minutes of school you will be able to graduate and get a High school diploma. To graduate use the command ?graduate')
         .setDescription(`You have ${education} minutes of school. To go to school use the command "?school`)
+        .addField('**Your graduation status is:**', graduation)
         message.channel.send(educationhours)
         
         }
