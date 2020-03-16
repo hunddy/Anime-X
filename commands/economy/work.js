d7c35b6ee33658789ebac2425f0b734c2540a73a
Hunddy <58757071+hunddy@users.noreply.github.com>
2020-03-16 11:21:04 -0700

    Update work.js

--- commands/economy/work.js
+++ commands/economy/work.js
@@ -5,12 +5,12 @@ module.exports = {
     run: async (client, message, args) => {
         const {Client, RichEmbed, Collection} = require('discord.js')
         const db = require('quick.db');
+        let graduation = await db.fetch(`highschooldiplomas_${message.author.id}`);
         if(!message.content.startsWith('?'))return; 
         
         if (args[0] == 'programmer') {
         let programmeramount = Math.floor(Math.random() * 500) + 1; // 1-500 random number. whatever you'd like
-        let programmerauthor = await db.fetch(`education_${message.author.id}`)
-        if (programmerauthor < 600) return message.channel.send(':rofl: You are too stupid. You need atleast `10H` of education to work here. (To get education use the command "?School")')
+        if (graduation === null) return message.channel.send(':rofl: You are too stupid. You need a high school diploma to work here. (To get education use the command "?School" then save up to graduate.)')
         let programmarembed = new RichEmbed()
         .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL) 
         .setDescription(`${message.author}, you worked as a programmer for epicgames, you fixed their game & earned ${programmeramount}$!`)
@@ -21,8 +21,7 @@ module.exports = {
         db.add(`animebucks_${message.author.id}`, programmeramount)
         } else if(args[0] == 'constructor') {
          let constructoramount = Math.floor(Math.random() * 200) + 1; // 1-500 random number. whatever you'd like
-        let constructorauthor = await db.fetch(`education_${message.author.id}`)
-        if (constructorauthor < 300) return message.channel.send(':rofl: You are too stupid. You need atleast `5H` of education to work here. (To get education use the command "?School")')
+         if (graduation === null) return message.channel.send(':rofl: You are too stupid. You need a high school diploma to work here. (To get education use the command "?School" then save up to graduate.)')
         let constructorembed = new RichEmbed()
         .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL) 
         .setDescription(`${message.author}, you worked as a constructor & got payed ${constructoramount}$ for building trumps wall!`)
@@ -33,8 +32,7 @@ module.exports = {
         db.add(`animebucks_${message.author.id}`, constructoramount)
         } else if(args[0] == 'cooker') {
         let cookeramount = Math.floor(Math.random() * 50) + 1;
-        let cookerauthor = await db.fetch(`education_${message.author.id}`)
-        if (cookerauthor < 300) return message.channel.send(':rofl: You are too stupid. You need atleast `5H` of education to work here. (To get education use the command "?School")')
+        if (graduation === null) return message.channel.send(':rofl: You are too stupid. You need a high school diploma to work here. (To get education use the command "?School" then save up to graduate.)')
         let cookerembed = new RichEmbed()
         .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL)
         .setDescription(`${message.author}, you worked and cooked for bill gates and got paid ${cookeramount}$`)
@@ -44,8 +42,7 @@ module.exports = {
         } else if (args[0] == 'policemen') {
                    
         let policemenamount = Math.floor(Math.random() * 300) + 1;
-        let policemenauthor = await db.fetch(`education_${message.author.id}`)
-        if (policemenauthor < 600) return message.channel.send(':rofl: You are too stupid. You need atleast `10H` of education to work here. (To get education use the command "?School")')
+        if (graduation === null) return message.channel.send(':rofl: You are too stupid. You need a high school diploma to work here. (To get education use the command "?School" then save up to graduate.)')
         let policemenembed = new RichEmbed()
         .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL)
         .setDescription(`${message.author}, you worked as a policemen and got paid ${policemenamount}$ for giving someone a ticket.`) 
