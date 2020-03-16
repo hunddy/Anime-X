da075dabf290a6312ae5b05a8c6310a0bd12360e
Hunddy <58757071+hunddy@users.noreply.github.com>
2020-03-15 17:01:44 -0700

    Update invite.js

--- commands/utility/invite.js
+++ commands/utility/invite.js
@@ -7,7 +7,7 @@ module.exports = {
         let inviteembed = new RichEmbed()
         .setColor("RANDOM")
         .setDescription("Here's a list of our invites where you can invite our bot or join our support server!")
-        .addField('**Need assistance or help with AnimeX? Join our support server!**', '[Join our support server!](https://discord.gg/R5JXU5h)')
+        .addField('**Need assistance or help with AnimeX? Join our support server!**', '[Join our support server!](https://discord.gg/5jYqNz2)')
         .addField('**Invite me to your server!**', '[Invite me to your server!](https://discordapp.com/oauth2/authorize?client_id=606941704204845088&scope=bot&permissions=2146958847&response_type=code)')
         message.channel.send(inviteembed)
         }
