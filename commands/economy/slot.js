2cacf4d0f757ca7e25dc18af589e6db6702f1d87
Hunddy <58757071+hunddy@users.noreply.github.com>
2020-03-17 09:40:02 -0700

    Update slot.js

--- commands/economy/slot.js
+++ commands/economy/slot.js
@@ -40,17 +40,20 @@ module.exports = {
     }
     if (win) {
         db.add(`animebucks_${message.author.id}`, money)
+        let currentwinbalance = db.fetch(`animebucks_${message.author.id}`)
         let slotsEmbed1 = new RichEmbed()
             .setTitle(`${user.username} <:Slots:679382050930819082> machine...`)
-            .setDescription(`>${slotItems[number[0]]} | ${slotItems[number[1]]} | ${slotItems[number[2]]} <\n\nYou won __**${money}**__ Animebucks!`)
+            .setDescription(`>${slotItems[number[0]]} | ${slotItems[number[1]]} | ${slotItems[number[2]]} <\n\nYou won __**${money}**__ Animebucks!\n\nYou know have ${currentwinbalance} Animebucks.`)
+           
             .setColor("RANDOM")
         message.channel.send(slotsEmbed1)
 
     } else {
         db.subtract(`animebucks_${message.author.id}`, money)
+        let currentlosebalance = db.fetch(`animebucks_${message.author.id}`)
         let slotsEmbed = new RichEmbed()
             .setTitle(`${user.username} <:Slots:679382050930819082> machine...`)
-            .setDescription(`>${slotItems[number[0]]} | ${slotItems[number[1]]} | ${slotItems[number[2]]} <\n\nYou lost __**${money}**__ Animebucks!`)
+            .setDescription(`>${slotItems[number[0]]} | ${slotItems[number[1]]} | ${slotItems[number[2]]} <\n\nYou lost __**${money}**__ Animebucks!\n\nYou know have ${currentlosebalance} Animebucks.`)
             .setColor("RANDOM")
         message.channel.send(slotsEmbed)
 
